3d9149e00952138139ffcee9933d26d5a6818b95
Celestin Niyindagiriye <nicelestin08@gmail.com>
2023-07-23 19:40:05 +0200

    chore: setup jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,19 @@
-import nextJest from "next/jest.js"
+// eslint-disable-next-line import/extensions
+import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  dir: "./",
-})
+  dir: './',
+});
 
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
-  testEnvironment: "jest-environment-jsdom",
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
-  testPathIgnorePatterns: ["<rootDir>/e2e"],
-}
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    './setupTests.js',
+  ],
+  testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  testPathIgnorePatterns: ['<rootDir>/e2e'],
+};
 
-export default createJestConfig(config)
+export default createJestConfig(config);
